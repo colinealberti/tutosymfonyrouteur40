0932829808169c5620eddd6a08873c5c4b074516
Coline ALBERTI <coline.alberti@gmail.com>
2020-11-25 16:51:46 +0100

    Début TP routage

---
+++ bandle.bat
@@ -0,0 +1,10 @@
+call composer require symfony/maker-bundle:1.23
+call composer require sensio/framework-extra-bundle 
+call composer require symfony/profiler-pack
+call composer require symfony/twig-bundle
+call composer require symfony/asset 
+call Composer require symfony/form			
+call Composer require symfony/validator 
+rem call Composer require symfony/orm-pack
+call composer require symfony/security-bundle
+
